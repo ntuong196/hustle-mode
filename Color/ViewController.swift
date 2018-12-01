eef6eb6a3cdbf810516b932c2a8dec2270b9940e
ntuong196 <stranger0411@gmail.com>
2018-12-02 05:10:38 +1000

    Add animation, button action, background change, play sound

--- Color/ViewController.swift
+++ Color/ViewController.swift
@@ -7,43 +7,77 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    var isPurple = false
+    
+    @IBOutlet var darkBlueBG: UIView!
+    @IBOutlet var powerButton: UIButton!
+    @IBOutlet var cloudHolder: UIView!
+    
+    
+    @IBOutlet var rocketStream: UIImageView!
+    @IBOutlet var launchMode: UILabel!
+    @IBOutlet var onMode: UILabel!
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-    }
-    @IBDesignable class IButton: UIButton
-    {
-        override func layoutSubviews() {
-            super.layoutSubviews()
-            
-            updateCornerRadius()
-        }
-        
-        @IBInspectable var rounded: Bool = false {
-            didSet {
-                updateCornerRadius()
-            }
-        }
-        
-        func updateCornerRadius() {
-            layer.cornerRadius = rounded ? frame.size.height / 2 : 0
-        }
-    }
-
-    @IBAction func changeMode(_ sender: Any) {
-        if isPurple {
-            view.backgroundColor = UIColor.brown
-            isPurple = false
-        } else {
-            view.backgroundColor = UIColor.purple
-            isPurple = true
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
         }
     }
     
+    @IBAction func launchAction(_ sender: Any) {
+        cloudHolder.isHidden = false
+        darkBlueBG.isHidden = false
+        powerButton.isHidden = true
+        
+        player.play()
+        
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocketStream.frame = CGRect(x: 0, y: 40, width: 375, height: 572)
+        }) { (finished) in
+            self.launchMode.isHidden = false
+            self.onMode.isHidden = false
+        }
+    }
+    
+//    @IBDesignable class IButton: UIButton
+//    {
+//        override func layoutSubviews() {
+//            super.layoutSubviews()
+//
+//            updateCornerRadius()
+//        }
+//
+//        @IBInspectable var rounded: Bool = false {
+//            didSet {
+//                updateCornerRadius()
+//            }
+//        }
+//
+//        func updateCornerRadius() {
+//            layer.cornerRadius = rounded ? frame.size.height / 2 : 0
+//        }
+//    }
+    
+//    var isPurple = false
+//    @IBAction func changeMode(_ sender: Any) {
+//        if isPurple {
+//            view.backgroundColor = UIColor.white
+//            isPurple = false
+//        } else {
+//            view.backgroundColor = UIColor.purple
+//            isPurple = true
+//        }
+//    }
 }
 
---
+++ README.md
@@ -0,0 +1,30 @@
+# Hustle Mode
+
+iOS App for changing application's mode.
+
+![Screenshot](https://github.com/ntuong196/hustle-mode/blob/master/Screenshots/launched.jpg)
+
+### System requirement:
+
+- Xcode 10
+- iOS 11 +
+
+### Feature:
+
+- Change Background Color
+- Change background design
+- Play sounds
+
+### TODO:
+
+- Add background image
+- Select mode
+- Restart launch
+
+### Fixed Error:
+
+- this class is not key value coding-compliant for the key updateRadius
+Fix: outlet name in storyboard did not match, delete outlet collection of button
+
+- cannot find media
+Fix: Using do ... catch
