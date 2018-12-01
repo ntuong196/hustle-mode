818e94e8c809a2483f9267abe17a77deca3b7eee
ntuong196 <stranger0411@gmail.com>
2018-12-02 02:08:17 +1000

    Initial Commit

---
+++ Color/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Color
+//
+//  Created by Steve on 2/12/18.
+//  Copyright © 2018 Steve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
